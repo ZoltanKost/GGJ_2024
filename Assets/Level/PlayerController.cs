12cdde002d8c53ac2002b5a56e4c4a2216a25033
Jonas Mumm <jonasmumm@web.de>
2024-01-27 16:27:49 +0100

    dialog box upon pick up

--- Assets/Level/PlayerController.cs
+++ Assets/Level/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
@@ -10,37 +11,64 @@ public class PlayerController : MonoBehaviour
     [SerializeField] KeyCode pickUp;
     [SerializeField] KeyCode inventoryOpen;
     [SerializeField] float radius;
+    [SerializeField] DialogBox dialogBox;
+
+    readonly HashSet<object> _inputBlockingObjects = new HashSet<object>();
 
     private void Update()
     {
         collect_PopUp.gameObject.SetActive(false);
-        if (Input.GetKeyDown(inventoryOpen))
+        if (_inputBlockingObjects.Count == 0)
         {
-            if (!InventoryUI.activeSelf)
+            if (Input.GetKeyDown(inventoryOpen))
             {
-                InventoryUI.SetActive(true);
+                if (!InventoryUI.activeSelf)
+                {
+                    InventoryUI.SetActive(true);
+                }
+                else
+                {
+                    InventoryUI.SetActive(false);
+                }
             }
-            else
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position, radius);
+            foreach (Collider2D collider in colliders)
             {
-                InventoryUI.SetActive(false);
-            }
-        }
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(transform.position,radius);
-        foreach(Collider2D collider in colliders)
-        {
-            if(collider.TryGetComponent(out Collectable component))
-            {
-                collect_PopUp.gameObject.SetActive(true);
-                if(Input.GetKeyDown(pickUp)){
-                    inventoryHandler.AddToInventory(component.GetSO());
-                    component.gameObject.SetActive(false);
+                if (collider.TryGetComponent(out Collectable component))
+                {
+                    collect_PopUp.gameObject.SetActive(true);
+                    if (Input.GetKeyDown(pickUp))
+                    {
+                        inventoryHandler.AddToInventory(component.GetSO());
+                        dialogBox.ShowDialog("You acuired half a joke!");
+                        component.gameObject.SetActive(false);
+                    }
                 }
             }
         }
     }
     private void FixedUpdate()
     {
-        var input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        rb.AddForce(input * movementForce);
+        if (_inputBlockingObjects.Count == 0)
+        {
+            var input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+            rb.AddForce(input * movementForce);
+        }
+    }
+
+    public void BlockInput(object key)
+    {
+        if(!_inputBlockingObjects.Add(key))
+        {
+            throw new System.Exception("Can't add key " + key.ToString());
+        }
+    }
+
+    public void UnblockInput(object key)
+    {
+        if (!_inputBlockingObjects.Remove(key))
+        {
+            throw new System.Exception("Can't remove key " + key.ToString());
+        }
     }
 }
