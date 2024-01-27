e14269ee997eedd78dfd049c9fbc9bc0473603db
ZoltanKost <mikle.mikle200@gmail.com>
2024-01-27 16:30:29 +0100

    Audio Manager

--- Assets/Scripts/CalculatorHandler.cs
+++ Assets/Scripts/CalculatorHandler.cs
@@ -5,9 +5,14 @@ public class CalculatorHandler : MonoBehaviour
     
     [SerializeField] private DropScript footer1;
     [SerializeField] private DropScript footer2;
+    [SerializeField] private AudioManager audioManager;
     JokePieceSO joke1;
     JokePieceSO joke2;
     public void Calculate(){
+        if(!footer1.occupied || !footer2.occupied){
+            return;
+        }
+        audioManager.ChangeClip(0);
         joke1 = footer1.GetJokeSO();
         joke2 = footer2.GetJokeSO();
         // int num = joke1.jokePiece_Points[joke2];
